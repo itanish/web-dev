3c85aec886431f01db569e29b39b07ea437bedbf
tanishsehgal <tanishsehgal1234@gmail.com>
2022-04-06 18:02:24 -0400

    a8 REST sp22

--- src/components/tuiter/TuitStats/tuit-stats.js
+++ src/components/tuiter/TuitStats/tuit-stats.js
@@ -1,5 +1,7 @@
 import {useDispatch} from "react-redux";
 import React from "react";
+import {findAllTuits, deleteTuit, createTuit, updateTuit}
+    from "../actions/tuits-actions";
 
 const TuitStats = ({tuit}) => {
     const dispatch = useDispatch();
@@ -11,17 +13,47 @@ const TuitStats = ({tuit}) => {
     <>
         <div className="row mt-2 ms-1">
 
-            <div className="col-3">
+            <div className="col-2">
                 <i className="fa-regular fa-comment wd-grid-col-bookmark-icons"></i>
                 <span className="wd-icon-count">{tuit.stats.comments}</span>
             </div>
 
-            <div className="col-3">
+            <div className="col-2">
                 <i className="fa-solid fa-retweet wd-grid-col-bookmark-icons"></i>
                 <span className="wd-icon-count">{tuit.stats.retuits}</span>
             </div>
 
-            <div className="col-3">
+            <div className="col-2">
+                <span onClick={() => updateTuit(dispatch, {
+                    ...tuit,
+                    likes: tuit.likes + 1
+                })}>
+                {
+                    <i className="fas fa-thumbs-up me-1"
+                       ></i>
+                }
+
+                    {tuit.likes}
+                </span>
+
+            </div>
+
+            <div className="col-2">
+                <span onClick={() => updateTuit(dispatch, {
+                    ...tuit,
+                    dislikes: tuit.dislikes + 1
+                })}>
+                {
+                    <i className="fas fa-thumbs-down me-1"
+                    ></i>
+                }
+
+                    {tuit.dislikes}
+                </span>
+
+            </div>
+
+            <div className="col-2">
                 <span onClick={likeTuit}>
                 {
                     tuit.liked &&
@@ -37,7 +69,7 @@ const TuitStats = ({tuit}) => {
 
             </div>
 
-            <div className="col-3">
+            <div className="col-2">
                 <i className="fa-solid fa-arrow-up-from-bracket wd-grid-col-bookmark-icons"></i>
             </div>
 
--- src/components/tuiter/tuit-list/index.js
+++ src/components/tuiter/tuit-list/index.js
@@ -43,22 +43,6 @@ const TuitList = () => {
                                                          post={tuit}/>
 
 
-                                                  <div>
-                                                      Likes: {tuit.likes}
-                                                      <i onClick={() => updateTuit(dispatch, {
-                                                          ...tuit,
-                                                          likes: tuit.likes + 1
-                                                      })} className="far fa-thumbs-up ms-2"></i>
-
-                                                  </div>
-                                                  <div>
-                                                      Dislikes: {tuit.dislikes}
-                                                      <i onClick={() => updateTuit(dispatch, {
-                                                          ...tuit,
-                                                          dislikes: tuit.dislikes + 1
-                                                      })} className="far fa-thumbs-down ms-2"></i>
-
-                                                  </div>
                                            <hr/>
                                               </div>
                                            </>
